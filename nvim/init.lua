c5ba68b98eb3cad57c5cd9a4fa7337b61c62c46d
vamsi-chivukula <vamsichivu@gmail.com>
2024-05-26 00:07:00 -0400

    added gitignore file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,3 @@
 -- global settings
 require("vamsi.core")
 require("vamsi.lazy")
-
--- vscode and nvim only settings
-if vim.g.vscode then
-	-- VSCode Extension
-	vim.cmd([[source $HOME/.config/nvim/vscode/settings.vim]])
-end
